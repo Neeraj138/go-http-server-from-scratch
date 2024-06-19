9733f162fe1d281cb8d6aadaa2e821ea7263e36b
Neeraj138 <neeraj.r@greyorange.com>
2024-06-19 18:34:42 +0530

    send 200 response

--- app/server.go
+++ app/server.go
@@ -13,9 +13,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	var resp string = "HTTP/1.1 200 OK\r\n\r\n"
+	conn.Write([]byte(resp))
+	conn.Close()
 }
