f0cdff2cd983e07ecac67242a548c3b3d0fd8ca5
Neeraj138 <neeraj.r@greyorange.com>
2024-06-20 20:42:46 +0530

    code-refactoring; capture request headers

--- app/server.go
+++ app/server.go
@@ -9,10 +9,21 @@ import (
 	"strings"
 )
 
+type Request struct {
+	method  string
+	url     string
+	headers map[string]string
+}
+
+func checkEchoSubpath(url string) (string, bool) {
+	echoSuffix, found := strings.CutPrefix(url, "/echo/")
+	return echoSuffix, found
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind to port 4221", err.Error())
 		os.Exit(1)
 	}
 
@@ -23,25 +34,54 @@ func main() {
 	}
 
 	reader := bufio.NewReader(conn)
-	line, _, err := reader.ReadLine()
-	if err != nil && err != io.EOF {
-		panic(err)
+	lineCnt := 0
+	temp := ""
+	request := Request{
+		method:  "",
+		url:     "",
+		headers: map[string]string{},
 	}
-	reqLine := strings.Fields(strings.TrimSpace(string(line)))
-	urlPath := ""
-	if len(reqLine) > 2 {
-		urlPath = reqLine[1]
-	}
-
+	headerKey := ""
 	resp := "HTTP/1.1 404 Not Found\r\n\r\n"
 
-	echoSuffix, foundEcho := strings.CutPrefix(urlPath, "/echo/")
-	if foundEcho {
-		resp = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoSuffix), echoSuffix)
-	} else if urlPath == "/" {
+	// reqProcessLoop:
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading the request", err.Error())
+		}
+		if len(line) == 0 {
+			break
+		}
+		temp = string(line)
+		fmt.Println(lineCnt, line, temp)
+
+		// request line
+		if lineCnt == 0 {
+			reqLine := strings.Fields(temp)
+			request.method = reqLine[0]
+			request.url = reqLine[1]
+		} else {
+			// capture request headers
+			colonInd := strings.IndexRune(temp, ':')
+			headerKey = temp[:colonInd]
+			request.headers[headerKey] = strings.TrimSpace(temp[colonInd+1:])
+		}
+
+		lineCnt++
+	}
+
+	if request.url == "/user-agent" || request.url == "/user-agent/" {
+		usrAgnt := request.headers["User-Agent"]
+		resp = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(usrAgnt), usrAgnt)
+	} else if echoSuff, found := checkEchoSubpath(request.url); found {
+		resp = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoSuff), echoSuff)
+	} else if request.url == "/" {
 		resp = "HTTP/1.1 200 OK\r\n\r\n"
 	}
-	fmt.Println(resp)
 
 	_, err = conn.Write([]byte(resp))
 	if err != nil {
@@ -52,4 +92,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Connection closed...")
 }
